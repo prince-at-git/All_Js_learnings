3c2e3be2f31b6751bd07183b197b38a497601fd0
NyXdeity <kiritorsp123456789@gmail.com>
2026-01-31 12:01:59 +0530

    added: bmi project

--- projects/BMI Calculator/Bindex.js
+++ projects/BMI Calculator/Bindex.js
@@ -30,8 +30,16 @@ form.addEventListener('submit', function(e){
     const weight = parseInt(document.querySelector('#weight').value)
     const result = document.querySelector('#result')
 
-    if(height === '' || height <=0 || isNaN(height)){
+    if( height <=0 || isNaN(height)){
         result.innerHTML = "Please inter a valid height"
+    } else if( weight <=0 || isNaN(weight)){
+        result.innerHTML = "Please inter a valid weight "
+    } else{
+        const bmi = (weight/((height*height)/10000)).toFixed(2)// to fixed:- to get value till 2 decimal point
+
+        // show the result
+        result.innerHTML = `<span>${bmi}</span>`
+
     }
 })
 
